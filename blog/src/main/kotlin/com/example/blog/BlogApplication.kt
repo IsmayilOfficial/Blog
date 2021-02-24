d64e6e741eb65d2dd65a8822542e1b06911fee32
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-02-24 18:02:26 +0400

    init

---
+++ blog/src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -0,0 +1,15 @@
+package com.example.blog
+
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogApplication>(*args)
+	{
+		setBannerMode(Banner.Mode.OFF)
+	}
+}
